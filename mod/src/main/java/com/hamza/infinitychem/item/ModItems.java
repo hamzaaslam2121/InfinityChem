3175c34b5f5157e91ab0122c38f31d01b50fdb47
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2022-07-31 21:51:21 +0100

    initizialized resources folder packages

---
+++ mod/src/main/java/com/hamza/infinitychem/item/ModItems.java
@@ -0,0 +1,16 @@
+package com.hamza.infinitychem.item;
+
+import com.hamza.infinitychem.InfinityChemMod;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, InfinityChemMod.MOD_ID);
+
+    public static void register(){
+        
+    }
+}
